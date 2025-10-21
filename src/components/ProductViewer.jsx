c3544e942e3f4db20752d36370d09424b881faad
aditya-2k23 <holaaditya123@gmail.com>
2025-10-21 17:38:17 +0530

    Add Canvas component and Macbook model rendering to ProductViewer

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,5 +1,8 @@
 import clsx from "clsx";
 import useMacbookStore from "../store";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -52,9 +55,18 @@ const ProductViewer = () => {
             </div>
           </div>
         </div>
-
-        <p className="text-white text-4xl">Render Canvas</p>
       </div>
+
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <ambientLight intensity={1} />
+
+        <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
+
+        <OrbitControls enableZoom={false} />
+      </Canvas>
     </section>
   );
 };
